9708033a1a7c671d3ada61e2911127b98ef4a3c6
joelBersek <jatamaria@unsa.edu.pe>
2024-05-31 18:39:38 -0500

    edit serializer in detail

--- apps/ticket_detail/serializers.py
+++ apps/ticket_detail/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from apps.ticket_detail.models import TicketDetail
 from apps.ticket.serializers import TicketSerializer
+from apps.product.serializers import ProductSerializer
 
 
 class TicketDetailSerializer(serializers.ModelSerializer):
     _ticket_id = TicketSerializer(source="ticket_id", read_only=True)
+    _product_id = ProductSerializer(source="product_id", read_only=True)
 
     class Meta:
         model = TicketDetail
