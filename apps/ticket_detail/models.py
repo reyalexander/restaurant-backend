ad3810ea7bbde4b1f948a60f2d6de85152583603
joelBersek <jatamaria@unsa.edu.pe>
2024-05-31 18:51:02 -0500

    fix

--- apps/ticket_detail/models.py
+++ apps/ticket_detail/models.py
@@ -2,19 +2,20 @@ from django.db import models
 from apps.ticket.models import Ticket
 from apps.product.models import Product
 
+
 class TicketDetail(models.Model):
     STATUS_CHOICES = (
-        (1, 'Activo'),
-        (2, 'Inactivo'),
-        (3, 'Eliminado'),
+        (1, "Activo"),
+        (2, "Inactivo"),
+        (3, "Eliminado"),
     )
-    ticket_id = models.ForeignKey(Ticket, on_delete= models.CASCADE)
-    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
+    ticket_id = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=True, blank=True)
-    product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
-    description = models.TextField(null=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField(null=True, blank=True)
+    status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
