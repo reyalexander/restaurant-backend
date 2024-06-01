f293929250c5c79f906dbae811a24cbf062db6ab
joelBersek <jatamaria@unsa.edu.pe>
2024-05-31 19:00:23 -0500

    reestruture Table

--- apps/table/models.py
+++ apps/table/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
+
 class Table(models.Model):
     STATUS_CHOICES = (
-        (1, 'Activo'),
-        (2, 'Inactivo'),
-        (3, 'Eliminado'),
+        (1, "Activo"),
+        (2, "Inactivo"),
+        (3, "Eliminado"),
     )
 
-    RESERVATION_CHOICES = (
-        (1, 'Sin reserva'),
-        (2, 'Reservado'),
-    )
-    
-    reservation = models.PositiveSmallIntegerField(choices=RESERVATION_CHOICES, default=1)
+    name = models.CharField(max_length=70, blank=True)
+    number = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, default=None)
     status = models.PositiveSmallIntegerField(choices=STATUS_CHOICES, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
     reservation_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-id']
+        ordering = ["-id"]
