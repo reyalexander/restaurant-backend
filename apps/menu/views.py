ee333c879e07a146909ab6701c82bfa17a3666a4
joelBersek <jatamaria@unsa.edu.pe>
2024-06-14 15:16:08 -0500

    fix queryset

--- apps/menu/views.py
+++ apps/menu/views.py
@@ -13,3 +13,19 @@ class MenuViewSet(viewsets.ModelViewSet):
     serializer_class = MenuSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [MenuViewFilter]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        if user.is_superuser:
+            return queryset.filter(
+                company_id=user.company_id
+            )  # El administrador puede ver todos los elementos, incluidos los eliminados
+        return queryset.filter(status__in=[1, 2], company_id=user.company_id)
+
+    def perform_create(self, serializer):
+        instance = serializer.save()
+        user = self.request.user
+        instance.company_id = user.company_id
+        instance.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
